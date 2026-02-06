41201cacf8a4bc932da612def24642a0a9cf2c8e
Rasha Basim <rasha18.dev@gmail.com>
2026-02-06 14:19:57 +0530

    3carterror fix

--- src/api/axios.js
+++ src/api/axios.js
@@ -4,12 +4,14 @@ const api = axios.create({
   baseURL: `${import.meta.env.VITE_API_URL}/api`,
 });
 
-// attach token automatically (for later)
+// ✅ attach token automatically
 api.interceptors.request.use((config) => {
-  const token = localStorage.getItem("token");
-  if (token) {
-    config.headers.Authorization = `Bearer ${token}`;
+  const userInfo = JSON.parse(localStorage.getItem("userInfo"));
+
+  if (userInfo?.token) {
+    config.headers.Authorization = `Bearer ${userInfo.token}`;
   }
+
   return config;
 });
 
